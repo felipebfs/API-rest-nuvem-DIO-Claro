40b35b8f8d21224642bd1d2ba979af03a8a478c4
Felipe Batista Fraga de Sousa <145801660+felipebfs@users.noreply.github.com>
2024-10-03 16:40:55 -0300

    Add files via upload

---
+++ src/main/java/br/com/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.domain.repository;
+
+import br.com.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String AccNumber);
+    boolean existsByCardNumber(String number);
+}
---
+++ src/main/java/br/com/service/UserService.java
@@ -0,0 +1,5 @@
+package br.com.service;
+
+import br.com.domain.model.User;
+
+public interface UserService extends CrudService<Long, User> {}
